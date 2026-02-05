720e344f583de8f02cdb3a840129d3010d459061
Gabriel Torres <gabrielst328@gmail.com>
2026-02-05 19:36:28 -0300

    feat: add validation to image uploader

--- components/ImageUploader/index.tsx
+++ components/ImageUploader/index.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { Button } from '@/components/Button';
+import { ErrorMessageComponent } from '@/components/ErrorMessage';
 import { ImageUpIcon } from 'lucide-react';
 import { useRef } from 'react';
+import { toast } from 'react-toastify';
 
 type ImageUploaderProps = {};
 
@@ -12,6 +14,22 @@ export function ImageUploader({}: ImageUploaderProps) {
   function handleChooseFile() {
     if (!fileInputRef.current) return;
     fileInputRef.current.click();
+  }
+
+  function handleChange() {
+    if (!fileInputRef.current) return;
+    const fileInput = fileInputRef.current;
+    const file = fileInput?.files?.[0];
+    if (!file) return;
+    if (file.size > 1000000) {
+      toast.error('Imagem muito grande. Tamanho máximo: 1MB');
+      fileInput.value = '';
+      return;
+    }
+    const formData = new FormData();
+    formData.append('file', file);
+    console.log(formData.get('file'));
+    fileInput.value = '';
   }
 
   return (
@@ -26,6 +44,7 @@ export function ImageUploader({}: ImageUploaderProps) {
         Enviar imagem de capa
       </Button>
       <input
+        onChange={handleChange}
         ref={fileInputRef}
         className="hidden"
         name="file"

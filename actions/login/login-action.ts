0c6c031f1068083bfa4997a91d4acf38af0c1808
Gabriel Torres <gabrielst328@gmail.com>
2026-02-11 15:01:24 -0300

    feat: implement login action with bcrypt

--- actions/login/login-action.ts
+++ actions/login/login-action.ts
@@ -1,7 +1,39 @@
 'use server';
 
+import { verifyPassword } from '@/lib/login/manage-login';
 import { asyncDelay } from '@/utils/simulate-delay';
 
-export async function loginAction() {
-  asyncDelay(3000);
+type LoginActionState = {
+  username: string;
+  error: string;
+};
+
+export async function loginAction(state: LoginActionState, formData: FormData) {
+  await asyncDelay(1000, true);
+
+  if (!(formData instanceof FormData)) {
+    return {
+      username: '',
+      error: 'Dados inválidos',
+    };
+  }
+
+  const username = formData.get('username')?.toString().trim() || '';
+  const isUsernameValid = username === process.env.LOGIN_USER;
+
+  const password = formData.get('password')?.toString().trim() || '';
+  const systemPassword = process.env.LOGIN_PASS?.toString() || '';
+  const isPasswordValid = await verifyPassword(password, systemPassword);
+
+  if (!isUsernameValid || !isPasswordValid) {
+    return {
+      username: username,
+      error: 'Nome de usuário ou senha incorretos',
+    };
+  }
+
+  return {
+    username: username,
+    error: '',
+  };
 }
--- components/admin/AdminLoginForm/index.tsx
+++ components/admin/AdminLoginForm/index.tsx
@@ -4,10 +4,22 @@ import { loginAction } from '@/actions/login/login-action';
 import { Button } from '@/components/Button';
 import { InputText } from '@/components/InputText';
 import { LogInIcon } from 'lucide-react';
-import { useActionState } from 'react';
+import { useActionState, useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 export function AdminLoginForm() {
-  const [state, action, isPending] = useActionState(loginAction, undefined);
+  const initialState = {
+    username: '',
+    error: '',
+  };
+  const [state, action, isPending] = useActionState(loginAction, initialState);
+
+  useEffect(() => {
+    if (state.error) {
+      toast.dismiss();
+      toast.error(state.error);
+    }
+  }, [state]);
 
   return (
     <div className="m-6 py-12 px-8 rounded-lg bg-slate-800">
@@ -15,21 +27,28 @@ export function AdminLoginForm() {
         <div className="flex flex-col gap-6">
           <InputText
             maxLength={64}
-            name="user"
+            type="text"
+            name="username"
             labeltext="Usuário"
             placeholder="Digite seu nome de usuário"
+            disabled={isPending}
+            defaultValue={state.username}
           />
           <InputText
             maxLength={64}
             name="password"
-            labeltext="Senha"
             type="password"
+            labeltext="Senha"
             placeholder="Digite sua senha"
+            disabled={isPending}
           />
           <Button className="flex gap-2" type="submit">
             <LogInIcon />
             <p>Entrar</p>
           </Button>
+          {!!state.error && (
+            <p className="text-red-400 text-center">{state.error}</p>
+          )}
         </div>
       </form>
     </div>
